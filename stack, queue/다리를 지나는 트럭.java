ce39cd8995b75749bedcb76f0d2263db96ff4058
myungjunlee <dlaudwns789@gmail.com>
2022-10-10 23:59:49 +0900

    add queue file

---
+++ stack, queue/다리를 지나는 트럭.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        int sum;
+        
+        Queue<Integer> queue = new LinkedList<>();
+        
+        for (int truck_weight : truck_weights) {
+            
+            if ((queue.size() < bridge_length) && (sum + truck_weight <= weight) {
+                    queue.offer(truck_weight);
+                    sum += truck_weight;
+                    answer += 1;
+            } else if ((queue.size() == bridge_length) || (sum + truck_weight > weight)) {
+                while(!queue.isEmpty) {
+                    sum -= queue.remove();
+                    if (sum + truck_weight <= weight) {
+                        queue.offer(truck_weight);
+                        sum += truck_weight;
+                        answer += 1;
+                        break;
+                    }
+                }
+            }
+            
+            if (queue.size() == 0) {
+                queue.offer(truck_weight);
+                sum = truck_weight;
+                answer += bridge_length;
+            }
+        }
+        
+        answer += 1
+        
+        return answer;
+    }
+}
